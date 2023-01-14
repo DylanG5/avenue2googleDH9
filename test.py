c7244282785501f31810259882d577b95d81f92d
Spuudz <bradbm1@mcmaster.ca>
2023-01-14 15:43:14 -0500

    Links directly to avenue and clicks login button

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
 DRIVER_PATH = '/path/to/chromedriver'
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-driver.get('https://google.com')
+driver.get('https://avenue.mcmaster.ca/?target=\%2Fd2l\%2Fhome')
+
+time.sleep(2)
+driver.find_element(By.XPATH,'/html/body/div/div[1]/ul/li[1]/a').click()
+time.sleep(1000000)
+driver.quit()
