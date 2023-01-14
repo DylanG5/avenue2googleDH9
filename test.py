a9e1e0a79bee63984f0a31a1989e1e86fbb8a655
Dylan Garner <dylan.garner@gmail.com>
2023-01-14 15:11:35 -0500

    testing for linux

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-import time
 DRIVER_PATH = '/path/to/chromedriver'
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-driver.get('https://nintendo.com')
-time.sleep(1000)
+driver.get('https://google.com')
