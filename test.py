113a2211492c97b753fa35c9c88c43b1c20086b7
Dylan Garner <dylan.garner@gmail.com>
2023-01-14 15:51:15 -0500

    testing new branch

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 from selenium import webdriver
+import time
 DRIVER_PATH = '/path/to/chromedriver'
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-driver.get('https://google.com')
+driver.get('https://avenue.mcmaster.ca/')
+email="garned3@mcmaster.ca"
+loginButton=driver.find_element("id","login_button")
+loginButton.click()
+uname = driver.find_element("id", "i0116")
