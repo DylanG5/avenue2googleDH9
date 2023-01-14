f7d3e83e2cb5ef764a9698ad422d4a3f3fe5e110
aswinku <aswin.kuga18@gmail.com>
2023-01-14 16:36:46 -0500

    Waits for 2FA before clicking the button

--- test.py
+++ test.py
@@ -12,8 +12,16 @@ driver.find_element(By.XPATH,'/html/body/div/div[1]/ul/li[1]/a').click()
 time.sleep(2)
 driver.find_element(By.XPATH,'/html/body/div/form[1]/div/div/div[2]/div[1]/div/div/div/div/div[1]/div[3]/div/div/div/div[2]/div[2]/div/input[1]').send_keys("kugana7@mcmaster.ca" + "\n")
 time.sleep(2)
-driver.find_element(By.XPATH,'/html/body/div/form[1]/div/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/div[2]/div/div[3]/div/div[2]/input').send_keys("PASSWORD" + "\n")
-time.sleep(10)
-driver.find_element(By.XPATH,'/html/body/div/form/div/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/div[2]/div/div[3]/div[2]/div/div/div[2]/input').click()
+driver.find_element(By.XPATH,'/html/body/div/form[1]/div/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/div[2]/div/div[3]/div/div[2]/input').send_keys("Password" + "\n")
+time.sleep(2)
+twofactor = True
+while twofactor: 
+    try:
+        driver.find_element(By.XPATH,'/html/body/div/form/div/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/div[2]/div/div[3]/div[2]/div/div/div[2]/input').click()
+        twofactor = False
+    except:
+        twofactor = True
+
+
 time.sleep(1000000)
 driver.quit()
