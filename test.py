40a6076a142697c8600e4c429410e4f8db81076c
aswinku <aswin.kuga18@gmail.com>
2023-01-14 14:35:31 -0500

    opened a test website using selenium

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print("hello world")
+from selenium import webdriver
+import time
+DRIVER_PATH = '/path/to/chromedriver'
+driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+driver.get('https://nintendo.com')
+time.sleep(1000)
